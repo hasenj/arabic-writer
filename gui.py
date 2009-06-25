ef8c2f89f6577a351b0b6489fcf0ff18b03cc70f
hasen j <hasan.aljudy@gmail.com>
2009-06-25 07:03:35 -0600

    using abs positioning

--- gui.py
+++ gui.py
@@ -2,15 +2,12 @@
 
 class MyFrame(wx.Frame):
     def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(250, 180))
+        wx.Frame.__init__(self, parent, id, title, size=(950, 580))
 
         panel = wx.Panel(self)
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        inp = wx.TextCtrl(panel)
-        oup = wx.TextCtrl(panel)
-        vbox.Add(inp)
-        vbox.Add(oup)
-        panel.SetSizer(vbox)
+
+        inp = wx.TextCtrl(panel, pos=(10, 10), size=(700, 200))
+        oup = wx.TextCtrl(panel, pos=(10, 220), size=(700, 200))
 
         self.Centre()
         self.Show(True)
