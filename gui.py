d477bd0f2e5c8dabab89d76d14aefa97f9761967
hasen j <hasan.aljudy@gmail.com>
2009-06-25 15:37:57 -0600

    added toggelable help text

--- gui.py
+++ gui.py
@@ -21,34 +21,51 @@ class MyFrame(wx.Frame):
 
         font = self.GetFont()
         font.SetPointSize(14)
+        font.SetFaceName("Arial")
+        font.SetWeight(wx.FONTWEIGHT_BOLD)
         self.SetFont(font)
 
         panel = wx.Panel(self)
 
-        inp_lbl = wx.StaticText(panel, pos=(10, 10), 
+        help_text_1 = wx.StaticText(panel, pos=(10, 10), 
                 label=u'Write Here - اكتب هنا')
         inp = wx.TextCtrl(panel, pos=(40, 40), size=(700, 200))
 
-        btn = wx.Button(panel, pos=(60, 540), size=(200, 80), label=u'Copy / نسخ')
-        reset_btn = wx.Button(panel, pos=(270, 540), size=(200, 80), label=u'Reset / مسح')
-        quit_btn = wx.Button(panel, pos=(540, 540), size=(200, 80), label=u'Quit / خروج')
+        btn = wx.Button(panel, pos=(60, 500), size=(200, 80), label=u'Copy / نسخ')
+        reset_btn = wx.Button(panel, pos=(270, 500), size=(200, 80), 
+                label=u'Reset / مسح')
+        quit_btn = wx.Button(panel, pos=(540, 500), size=(200, 80), 
+                label=u'Quit / خروج')
+
+        help_btn = wx.Button(panel, pos=(120, 600), size=(500, 40), 
+                label=u'Help / تعليمات')
+
+        help_text_arabic = wx.StaticText(panel, pos=(90, 280), size=(700, 100),
+                label=u""" ثم اضغط زر النسخ, و بعدها يمكنك لصق النص في اي برنامج لا يدعم العربية """) 
+        help_text_english = wx.StaticText(panel, pos=(50, 330), size=(700, 100),
+                label=u""" Then press "Copy", and then go and paste it in a program that doesn't support Arabic properly """) 
+
+        help_labels = [help_text_1, help_text_arabic, help_text_english]
+
+        def toggle_help(evt=None):
+            for label in help_labels:
+                label.Show(not label.IsShown())
+        help_btn.Bind(wx.EVT_BUTTON, toggle_help)
+        toggle_help()
 
         def copy(evt):
             rtl = rtlize(inp.GetValue())
             copied = CopyText(rtl)
-
         btn.Bind(wx.EVT_BUTTON, copy)
 
         def reset(evt):
             inp.SelectAll()
             inp.Clear()
             inp.SetSelection(0,0)
-
         reset_btn.Bind(wx.EVT_BUTTON, reset)
 
         def quit(evt):
             self.Close()
-
         quit_btn.Bind(wx.EVT_BUTTON, quit)
 
         self.Centre()
