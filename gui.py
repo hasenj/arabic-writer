7f7e23e5b5d82354d63838772d63e290dd3f8a07
hasen j <hasan.aljudy@gmail.com>
2009-06-25 07:10:48 -0600

    added a copy button (doesn't work yet)

--- gui.py
+++ gui.py
@@ -3,12 +3,14 @@
 class MyFrame(wx.Frame):
     def __init__(self, parent, id, title):
         style = wx.MAXIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX
-        wx.Frame.__init__(self, parent, id, title, size=(950, 580), style = style)
+        wx.Frame.__init__(self, parent, id, title, size=(750, 700), style = style)
 
         panel = wx.Panel(self)
 
         inp = wx.TextCtrl(panel, pos=(10, 10), size=(700, 200))
         oup = wx.TextCtrl(panel, pos=(10, 220), size=(700, 200))
+
+        btn = wx.Button(panel, pos=(20, 450), size=(200, 80), label=u'نسخ')
 
         self.Centre()
         self.Show(True)
