a6cac3feaecb7b3f1134c51da425d480f2495e8e
hasen j <hasan.aljudy@gmail.com>
2009-06-25 07:05:30 -0600

    window doesn't resize

--- gui.py
+++ gui.py
@@ -2,7 +2,8 @@
 
 class MyFrame(wx.Frame):
     def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(950, 580))
+        style = wx.MAXIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX
+        wx.Frame.__init__(self, parent, id, title, size=(950, 580), style = style)
 
         panel = wx.Panel(self)
 
