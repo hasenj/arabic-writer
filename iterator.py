68be163a975a32b0d041282b3b754b787cd61386
hasen j <hasan.aljudy@gmail.com>
2009-06-25 15:03:00 -0600

    style changes

--- iterator.py
+++ iterator.py
@@ -11,15 +11,15 @@ class Iterator:
         self.i = 0
     def get(self):
         return self.list[self.i]
-    def in_range( self, index = None ):
+    def in_range(self, index = None):
         if index == None: index = self.i
         return index in range(0,len(self.list))
-    def peek_index( self, index ):
-        if self.in_range( index ) : return self.list[index]
+    def peek_index(self, index):
+        if self.in_range(index) : return self.list[index]
         else: return None        
     def peek_next(self):
-        return self.peek_index( self.i + 1 )
+        return self.peek_index(self.i + 1)
     def peek_prev(self):
-        return self.peek_index( self.i - 1 )
+        return self.peek_index(self.i - 1)
     def move(self):
         self.i = self.i+1        
--- segments.py
+++ segments.py
@@ -10,22 +10,22 @@ class SegmentBuilder:
         segments = []
         iter = Iterator(string)
         while iter.in_range():
-            segments.append( self.build_segment( iter ) )
+            segments.append(self.build_segment(iter))
         return segments
         
-    def get_dir( self, letter ):
-        if Harf.is_harf( letter ): return "R" #only checks for arabic characterss .. other right-to-left languages are not checked
+    def get_dir(self, letter):
+        if Harf.is_harf(letter): return "R" #only checks for arabic characterss .. other right-to-left languages are not checked
         elif letter.isalpha(): return "L"
         else: return "N"        
 
 class DefaultSegmentBuilder(SegmentBuilder):
     def build_segment(self, iterator):
-        dir = self.get_dir( iterator.get() )
+        dir = self.get_dir(iterator.get())
         str = u""
-        while iterator.in_range() and self.get_dir( iterator.get() ) == dir:
-            str = str + ( iterator.get() )
+        while iterator.in_range() and self.get_dir(iterator.get()) == dir:
+            str = str + (iterator.get())
             iterator.move()
-        return Segment( str, dir )                
+        return Segment(str, dir)                
 
 class Segment:
     def __init__(self, str, dir):
@@ -38,14 +38,14 @@ class Segment:
         if self.dir == "R":
             #set shapings
             shaped = u""
-            iter = Iterator( self.str )
+            iter = Iterator(self.str)
             while iter.in_range():
                 #get Harf objects for the surrounding letters
-                prev = Harf.get( iter.peek_prev() )
-                current = Harf.get( iter.get() )
-                next = Harf.get( iter.peek_next() )
+                prev = Harf.get(iter.peek_prev())
+                current = Harf.get(iter.get())
+                next = Harf.get(iter.peek_next())
                 #get the shape for the current letter
-                shaped = shaped + current.get_contextual_shape( prev, next )
+                shaped = shaped + current.get_contextual_shape(prev, next)
                 iter.move()
             return shaped[::-1] #return reversed version .. 
         else:
