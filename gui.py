e86dea69647a9895977f5451a0737bc7d4efac79
hasen j <hasan.aljudy@gmail.com>
2009-06-25 06:47:47 -0600

    added BOM

--- gui.py
+++ gui.py
@@ -1,17 +1,17 @@
-import wx
+﻿import wx
 
 class MyFrame(wx.Frame):
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title, size=(250, 180))
 
         vbox = wx.BoxSizer(wx.VERTICAL)
-        inp = wx.TextCtrl(vbox, -1)
-        oup = wx.TextCtrl(vbox, -1)
+        inp = wx.TextCtrl(self)
+        oup = wx.TextCtrl(self)
 
         self.Centre()
         self.Show(True)
 
 app = wx.App(0)
-MyFrame(None, -1, 'Free(dom) Writer') 
+MyFrame(None, -1, u'الرسم الحر')
 app.MainLoop()
 
