8c5d2960e956eb7f70d55bf054ba808e6b3d0f8d
hasen j <hasan.aljudy@gmail.com>
2009-06-26 20:48:56 -0600

    separated arabic and english text

---
+++ free_rassa_eng.py
@@ -0,0 +1,4 @@
+import gui
+
+gui.language = 'english'
+gui.main()
--- gui.py
+++ gui.py
@@ -5,6 +5,29 @@
 import wx
 from process import rtlize
 
+language = 'arabic'
+
+t_text = dict(
+        arabic = dict(
+            copy = u'نسخ',
+            reset = u'مسح',
+            quit = u'خروج',
+            help = u'تعليمات',
+            help_info = u'اكتب اي شيء في مربع النصوص ثم اضغط زر النسخ و بعدها السق النص في اي برنامج لا يدعم اللغة العربية',
+            ),
+        english = dict(
+            copy = 'Copy',
+            reset = 'Reset',
+            quit = 'Quit',
+            help = 'Help',
+            help_info = '''Write some Arabic text in the text box, then press Copy. A processed version of the text will be copied to the clipboard, which you can use to paste into any program that doesn't properly show Arabic text.
+            ''',
+                   )
+        )
+
+def get_text(name):
+    return t_text[language][name]
+
 def CopyText(text):
     """Set text into the system clipboard"""
     ok = False
@@ -27,26 +50,18 @@ class MyFrame(wx.Frame):
 
         panel = wx.Panel(self)
 
-        help_text_1 = wx.StaticText(panel, pos=(300, 10), 
-                label=u'Write Here - اكتب هنا')
         inp = wx.TextCtrl(panel, pos=(40, 40), size=(700, 200))
 
-        btn = wx.Button(panel, pos=(60, 400), size=(200, 80), label=u'Copy / نسخ')
+        btn = wx.Button(panel, pos=(60, 400), size=(200, 80), label=get_text("copy"))
         reset_btn = wx.Button(panel, pos=(270, 400), size=(200, 80), 
-                label=u'Reset / مسح')
+                label=get_text("reset"))
         quit_btn = wx.Button(panel, pos=(540, 400), size=(200, 80), 
-                label=u'Quit / خروج')
-
+                label=get_text("quit"))
         help_btn = wx.Button(panel, pos=(150, 500), size=(500, 40), 
-                label=u'Help / تعليمات')
-
-        help_text_2 = wx.StaticText(panel, pos=(50, 240), size=(700, 150),
-                label=u"""
-ثم اضغط زر النسخ, و بعدها يمكنك لصق النص في اي برنامج لا يدعم العربية 
-                
-Then press "Copy", and then go and paste it in any program that doesn't support Arabic properly """) 
-
-        help_labels = [help_text_1, help_text_2]
+                label=get_text("help")) 
+        help_text = wx.StaticText(panel, pos=(50, 240), size=(700, 150),
+                label=get_text("help_info"))
+        help_labels = [help_text]
 
         def toggle_help(evt=None):
             for label in help_labels:
@@ -72,7 +87,10 @@ Then press "Copy", and then go and paste it in any program that doesn't support 
         self.Centre()
         self.Show(True)
 
-app = wx.App(0)
-MyFrame(None, -1, u'الرسام الحر')
-app.MainLoop()
+def main():
+    app = wx.App(0)
+    MyFrame(None, -1, u'الرسام الحر')
+    app.MainLoop()
+
+if __name__ == '__main__': main()
 
