df274ab98edf36b6a6303938b7c05d51fe877021
hasen j <hasan.aljudy@gmail.com>
2009-07-02 04:10:53 -0600

    qt shaping up, but clipboard problem

--- qt.py
+++ qt.py
@@ -29,7 +29,16 @@ def get_text(name):
     """Basic (read: naive) mechanism for multi-language UI"""
     return t_text[language][name]
 
+
+
 app = QtGui.QApplication(sys.argv)
+
+# XXX: doesn't work right ..
+def setClipboardText(text):
+    clipboard = app.clipboard()
+    clipboard.setText(text)
+    event = QtCore.QEvent(QtCore.QEvent.Clipboard)
+    app.sendEvent(clipboard, event)
 
 window = QtGui.QWidget()
 window.resize(250, 150)
@@ -39,18 +48,30 @@ window.setWindowIcon(QtGui.QIcon('art/icon.png'))
 vbox = QtGui.QVBoxLayout()
 hbox = QtGui.QHBoxLayout()
 
-quit_btn = QtGui.QPushButton(get_text('quit'))
-window.connect(quit_btn, QtCore.SIGNAL('clicked()'), app, QtCore.SLOT('quit()'))
 textArea = QtGui.QTextEdit()
 
+quitBtn = QtGui.QPushButton(get_text('quit'))
+window.connect(quitBtn, QtCore.SIGNAL('clicked()'), app, QtCore.SLOT('quit()'))
+
+copyBtn = QtGui.QPushButton(get_text('copy'))
+def copy():
+    text = unicode(textArea.toPlainText())
+    rtl = rtlize(text)
+    setClipboardText(rtl)
+window.connect(quitBtn, QtCore.SIGNAL('clicked()'), copy)
+
+
+hbox.addWidget(copyBtn)
 hbox.addStretch(1)
-hbox.addWidget(quit_btn)
+hbox.addWidget(quitBtn)
 
 vbox.addWidget(textArea)
+vbox.addSpacing(20)
 vbox.addLayout(hbox)
 
 window.setLayout(vbox)
 window.show()
 
-sys.exit(app.exec_())
+exit_code = app.exec_()
+# sys.exit(exit_code)
 
