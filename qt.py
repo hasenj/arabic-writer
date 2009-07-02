a1c00fca3422ae9b89b2a40cc8aa088e39823146
hasen j <hasan.aljudy@gmail.com>
2009-07-02 05:34:28 -0600

    added inplace processing

--- qt.py
+++ qt.py
@@ -2,6 +2,7 @@
 import sys
 import webbrowser
 from process import rtlize
+from clipboard import setcbtext
 from PyQt4 import QtGui, QtCore
 
 language = 'arabic'
@@ -11,6 +12,7 @@ t_text = dict(
             title = u'الرسام الحر',
             copy = u'نسخ',
             reset = u'مسح',
+            inplace = u'معالجة النص في مكانه',
             quit = u'خروج',
             help = u'تعليمات',
             help_doc = 'help_arabic.html',
@@ -19,6 +21,7 @@ t_text = dict(
             title = 'The Free Ressam (Er Ressam il Hur)',
             copy = 'Copy',
             reset = 'Reset',
+            inplace = 'Process InPlace',
             quit = 'Quit',
             help = 'Help',
             help_doc = 'help_english.html',
@@ -39,33 +42,51 @@ def setClipboardText(text):
     clipboard.setText(text)
     event = QtCore.QEvent(QtCore.QEvent.Clipboard)
     app.sendEvent(clipboard, event)
+    app.processEvents()
 
 window = QtGui.QWidget()
-window.resize(250, 150)
+window.resize(450, 250)
 window.setWindowTitle('Qt Window')
 window.setWindowIcon(QtGui.QIcon('art/icon.png'))
 
 vbox = QtGui.QVBoxLayout()
 hbox = QtGui.QHBoxLayout()
+edithbox = QtGui.QHBoxLayout()
 
 textArea = QtGui.QTextEdit()
+smallBar = QtGui.QToolBar()
+# smallBar.setOrientation(QtGui.QOrientation.Vertical)
 
-quitBtn = QtGui.QPushButton(get_text('quit'))
-window.connect(quitBtn, QtCore.SIGNAL('clicked()'), app, QtCore.SLOT('quit()'))
-
-copyBtn = QtGui.QPushButton(get_text('copy'))
 def copy():
     text = unicode(textArea.toPlainText())
     rtl = rtlize(text)
     setClipboardText(rtl)
+
+def process_inplace():
+    text = unicode(textArea.toPlainText())
+    rtl = rtlize(text)
+    textArea.setText(rtl)
+quitBtn = QtGui.QPushButton(get_text('quit'))
+window.connect(quitBtn, QtCore.SIGNAL('clicked()'), app, QtCore.SLOT('quit()'))
+
+copyBtn = QtGui.QPushButton(get_text('copy'))
 window.connect(quitBtn, QtCore.SIGNAL('clicked()'), copy)
 
+clearAction = QtGui.QAction(QtGui.QIcon('art/clear.png'), get_text('reset'), smallBar)
+window.connect(clearAction, QtCore.SIGNAL('triggered()'), textArea, QtCore.SLOT('clear()'))
+inplaceAction = QtGui.QAction(QtGui.QIcon('art/inplace.png'), get_text('inplace'), smallBar)
+window.connect(inplaceAction, QtCore.SIGNAL('triggered()'), process_inplace)
+smallBar.addAction(clearAction)
+smallBar.addAction(inplaceAction)
+
+edithbox.addWidget(textArea)
+edithbox.addWidget(smallBar)
 
 hbox.addWidget(copyBtn)
 hbox.addStretch(1)
 hbox.addWidget(quitBtn)
 
-vbox.addWidget(textArea)
+vbox.addLayout(edithbox)
 vbox.addSpacing(20)
 vbox.addLayout(hbox)
 
